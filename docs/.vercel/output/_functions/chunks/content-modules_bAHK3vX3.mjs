23af4668ae6f0db1dcd76f698b17419f18f65bdc
RATIU5 <jcmemmott20@outlook.com>
2025-12-27 09:05:28 -0700

    rm .vercel directory

--- docs/.vercel/output/_functions/chunks/content-modules_bAHK3vX3.mjs
+++
@@ -1,4 +0,0 @@
-const contentModules = new Map([
-["src/content/docs/index.mdx", () => import('./index_-44Vwso1.mjs')]]);
-
-export { contentModules as default };
--- docs/.vercel/output/_functions/renderers.mjs
+++
@@ -1,1 +0,0 @@
-export { r as renderers } from './chunks/_@astro-renderers_BCHh4Q3R.mjs';
